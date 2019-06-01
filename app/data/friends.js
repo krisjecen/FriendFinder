505e689f8227580f4d0600d60e4718a1a0a671ac
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-05-31 22:12:07 -0700

    added basic comparison functionality with simple arrays to test absolute value calculation and total difference calculation for user survey data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,57 @@
 // survey data
-var surveyArray = [];
+var surveyArray = [
+    {
+        "name":"James",
+        "photo":"https://cdn.pixabay.com/photo/2017/06/28/04/29/board-2449726_960_720.jpg",
+        "scores":[
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+            ]
+
+    },
+    {
+        "name":"Fredrica",
+        "photo":"https://cdn.pixabay.com/photo/2017/11/29/09/15/paint-2985569_960_720.jpg",
+        "scores":[
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+            ]
+
+    },
+    {
+        "name":"Carmell",
+        "photo":"https://cdn.pixabay.com/photo/2015/01/01/22/15/woman-586185_960_720.jpg",
+        "scores":[
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+            ]
+
+    }
+];
 // object structure: one object per user survey
 // each object in the array will be pushed into surveyArray
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,6 +3,25 @@
 
 var surveyData = require("../data/friends");
 
+// function for calculating compatibility
+function computeBestMatch(userScores){
+    // get just the scores part of the user's data
+    console.log("within the function: " + userScores);
+    // compare the scores with the scores of other users
+    var matchDifference = 0;
+    for (let p = 0; p < surveyData.length; p++) {
+        matchDifference = 0;
+        console.log(`currently we are comparing with: ${surveyData[p].name}`)
+        for (let i = 0; i < userScores.length; i++) {
+            // walk through the arrays and compare
+            matchDifference += Math.abs(userScores[i] - surveyData[p].scores[i]);
+            console.log(`current match difference: ${matchDifference}`)
+        };
+        console.log(`the match difference for ${surveyData[p].name} is ${matchDifference}.`)
+    };
+}
+
+
 module.exports = function(app) {
     // GET route to /api/friends
     // displays the JSON object containing all stored survey data
@@ -14,6 +33,12 @@ module.exports = function(app) {
     // POST route to /api/friends
     // takes survey data entered on the survey page
     app.post("/api/friends", function(req, res) {
+        // let something equal req.body that we can pass into our compute function?
+        var userScores = req.body.scores;
+        console.log(`Here is where i'm trying to capture the scores just entered (the request):
+        --->${userScores}`);
+        console.log(`the length of the scores array is ${userScores.length}`)
+        computeBestMatch(userScores)
         surveyData.push(req.body);
         res.json(surveyData);
         console.log(surveyData);
