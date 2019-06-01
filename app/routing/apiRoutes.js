1473cfed06a91d64bbd62e75da920d9849d85a72
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-05-31 22:49:52 -0700

    added functionality to determine which of the friends listed in the survey data array is the best match (lowest difference in survey scores) for the user

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,22 +3,32 @@
 
 var surveyData = require("../data/friends");
 
+// i moved all of this function into the post request/response part below due to variable scope issues
 // function for calculating compatibility
 function computeBestMatch(userScores){
     // get just the scores part of the user's data
-    console.log("within the function: " + userScores);
+    // console.log("within the function: " + userScores);
     // compare the scores with the scores of other users
     var matchDifference = 0;
+    var lowestDifference = Infinity;
+    var bestMatch = null;
     for (let p = 0; p < surveyData.length; p++) {
         matchDifference = 0;
         console.log(`currently we are comparing with: ${surveyData[p].name}`)
         for (let i = 0; i < userScores.length; i++) {
             // walk through the arrays and compare
             matchDifference += Math.abs(userScores[i] - surveyData[p].scores[i]);
-            console.log(`current match difference: ${matchDifference}`)
+            // console.log(`current match difference: ${matchDifference}`)
         };
         console.log(`the match difference for ${surveyData[p].name} is ${matchDifference}.`)
+        if (matchDifference < lowestDifference) {
+            lowestDifference = matchDifference;
+            bestMatch = surveyData[p].name;
+            console.log(`the best match so far is ${bestMatch} with ${lowestDifference}.`)
+
+        };
     };
+    // return bestMatch;
 }
 
 
@@ -35,10 +45,30 @@ module.exports = function(app) {
     app.post("/api/friends", function(req, res) {
         // let something equal req.body that we can pass into our compute function?
         var userScores = req.body.scores;
-        console.log(`Here is where i'm trying to capture the scores just entered (the request):
-        --->${userScores}`);
-        console.log(`the length of the scores array is ${userScores.length}`)
-        computeBestMatch(userScores)
+        // console.log(`Here is where i'm trying to capture the scores just entered (the request):
+        // --->${userScores}`);
+        // console.log(`the length of the scores array is ${userScores.length}`)
+        // computeBestMatch(userScores)
+        var matchDifference = 0;
+        var lowestDifference = Infinity;
+        var bestMatch = null;
+        for (let p = 0; p < surveyData.length; p++) {
+            matchDifference = 0;
+            console.log(`currently we are comparing with: ${surveyData[p].name}`)
+            for (let i = 0; i < userScores.length; i++) {
+                // walk through the arrays and compare
+                matchDifference += Math.abs(userScores[i] - surveyData[p].scores[i]);
+                // console.log(`current match difference: ${matchDifference}`)
+            };
+            console.log(`the match difference for ${surveyData[p].name} is ${matchDifference}.`)
+            if (matchDifference < lowestDifference) {
+                lowestDifference = matchDifference;
+                bestMatch = surveyData[p].name;
+                console.log(`the best match so far is ${bestMatch} with ${lowestDifference}.`)
+    
+            };
+        };
+        console.log(`can i see ${bestMatch} out here?`)
         surveyData.push(req.body);
         res.json(surveyData);
         console.log(surveyData);
