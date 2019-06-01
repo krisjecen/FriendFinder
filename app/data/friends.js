821487ee55468e983e393d4a8238d522a80fd6f2
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-05-31 20:33:33 -0700

    added ability to save user survey data and functionality to push that data to the data array

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // survey data
 var surveyArray = [];
 // object structure: one object per user survey
-// each object in the array will be pushed into the surveyData array
+// each object in the array will be pushed into surveyArray
 
 // export this data so other our other files can use it
 module.exports = surveyArray;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,14 +3,19 @@
 
 var surveyData = require("../data/friends");
 
-
 module.exports = function(app) {
     // GET route to /api/friends
     // displays the JSON object containing all stored survey data
     app.get("/api/friends", function(req, res) {
+        console.log("is this where it's reading?")
         res.json(surveyData);
     });
 
     // POST route to /api/friends
     // takes survey data entered on the survey page
+    app.post("/api/friends", function(req, res) {
+        surveyData.push(req.body);
+        res.json(surveyData);
+        console.log(surveyData);
+    })
 };
